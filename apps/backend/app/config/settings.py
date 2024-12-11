9fb3ccef54ff62b6777c31c275f660cf7bd6cc1f
tabei-dev <tabei.dev@gmail.com>
2024-12-11 16:13:17 +0900

    バックエンドで.envを読み込むためにpipenvを使用するよう改修

--- apps/backend/app/config/settings.py
+++ apps/backend/app/config/settings.py
@@ -1,10 +1,13 @@
 import os
 from pydantic_settings import BaseSettings
 from pydantic import ConfigDict
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
-    allow_origins: str = "http://example.com"
-    database_url: str = "sqlite:///./test.db"
+    allow_origins: str = os.getenv("ALLOW_ORIGINS", "http://example.com")
+    database_url: str = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
     model_config = ConfigDict(env_file=os.path.join(os.path.dirname(__file__), '../..', '.env'))
 
