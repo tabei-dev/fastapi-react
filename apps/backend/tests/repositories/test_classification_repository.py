ed4b9bc59768db9576cf8d45f96b543bc8a7990c
tabei-itscorp <tabei@its-corp.co.jp>
2025-01-22 15:25:45 +0900

    メッセージ情報と区分情報を取得するテストをモックを使わずに実施するよう修正

--- apps/backend/tests/repositories/test_classification_repository.py
+++ apps/backend/tests/repositories/test_classification_repository.py
@@ -15,7 +15,7 @@ def get_classification_details_success():
 def get_classification_details_failure():
     with pytest.raises(AssertionError) as exc_info:
         get_classification_details("INVALID")
-    # assert f"区分列挙型(INVALID)に該当する区分明細情報が見つかりませんでした" in str(exc_info.value)
+    assert f"区分列挙型(INVALID)に該当する区分明細情報が見つかりませんでした" in str(exc_info.value)
 
 def get_classification_detail_success():
     # 区分情報は静的データのため、実データをテストデータとして使用します
@@ -25,4 +25,4 @@ def get_classification_detail_success():
 def get_classification_detail_failure():
     with pytest.raises(AssertionError) as exc_info:
         get_classification_detail(ClassificationEnum.ROLE, '9999')
-    # assert f"区分列挙型(ROLE)の区分明細情報(9999)が見つかりませんでした" in str(exc_info.value)
+    assert f"区分列挙型(ROLE)の区分明細情報(9999)が見つかりませんでした" in str(exc_info.value)
